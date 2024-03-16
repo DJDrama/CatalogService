14f0eac308a40ccf66e1294ac3f96c2b0469aac8
DJDrama <putzzang@naver.com>
2024-03-17 02:19:43 +0900

    spring boot setup

---
+++ src/main/kotlin/kotlinspring/controller/GreetingController.kt
@@ -0,0 +1,24 @@
+package kotlinspring.controller
+
+import io.github.oshai.kotlinlogging.*
+import kotlinspring.service.GreetingService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/greetings")
+class GreetingController(
+    private val greetingsService: GreetingService
+) {
+
+    private val logger = KotlinLogging.logger {  }
+    @GetMapping("/{name}")
+    fun retrieveGreeting(@PathVariable("name") name: String): String {
+        logger.info { "hello world $name" }
+        return greetingsService.retrieveGreeting(name = name)
+    }
+
+
+}
---
+++ src/test/kotlin/kotlinspring/CourseCatalogServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotlinspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseCatalogServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
